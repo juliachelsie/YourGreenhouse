2601929219ac5b8c6b968b8e4bf338427517d0a6
juliachelsie Gustafsson <juliachelsiegustafsson@gmail.com>
2024-04-16 15:16:07 +0000

    Installed summernote and made first model

--- blogsite/admin.py
+++ blogsite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post, CommentOn
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
 
 # Register your models here.
--- blogsite/models.py
+++ blogsite/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    updated = models.DateTimeField(auto_now=True)
+    chosen_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, blank=True, related_name="user_likes")
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def likes_number(self):
+        return self.ikes.count()
+
+class CommentOn(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    body = models.TextField()
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
