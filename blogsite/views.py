22c25a3c109a85b897f73bd7a1c9bc8592365ccf
juliachelsie Gustafsson <juliachelsiegustafsson@gmail.com>
2024-04-22 11:21:56 +0000

    Added function to comment on post

--- blogsite/views.py
+++ blogsite/views.py
@@ -33,6 +33,39 @@ class Details(View):
             {
                 "post":post,
                 "comment": comment,
+                "commented": False,
+                "liked": liked,
+                "comment_on_form" : CommentOnForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = post.comment.filter(approved=True).order_by('created')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_on_form = CommentOnForm(data=request.POST)
+
+        if comment_on_form.is_valid():
+            comment_on_form.instance.email = request.user.email
+            comment_on_form.instance.name = request.user.username
+            comment = comment_on_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_on_form = CommentOnForm()
+
+
+        return render(
+            request,
+            "details.html",
+            {
+                "post":post,
+                "comment": comment,
+                "commented": True,
                 "liked": liked,
                 "comment_on_form" : CommentOnForm()
             },
